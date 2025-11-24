84a09b63d7de17efbbae5f2d4d9904282cbc306e
sandeep <sandeepkahawatta9@gmail.com>
2025-11-24 15:53:16 +0530

    Add the logic to project raw inputs  into a common hidden dimension.

--- gnn_optimizer/src/models/hgat_core.py
+++ gnn_optimizer/src/models/hgat_core.py
@@ -6,8 +6,14 @@ from torch_geometric.nn import HeteroConv, GATConv, Linear
 class RecurrentHGAT(nn.Module):
     def __init__(self, hidden_channels, out_channels, num_heads, metadata):
         super().__init__()
+
         self.hidden_channels = hidden_channels
-        self.metadata = metadata
+        
+        # 1. Input Encoders (Feature Projection) 
+        # Project varying input sizes to a common hidden dimension
+        self.encoder_dict = nn.ModuleDict()
+        self.encoder_dict['intersection'] = Linear(-1, hidden_channels)
+        self.encoder_dict['lane'] = Linear(-1, hidden_channels)
         
         # Layers will be defined in future commits
         
