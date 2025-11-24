38b59237490f78446aa4d4f28110007ad8b8713a
sandeep <sandeepkahawatta9@gmail.com>
2025-11-24 20:03:21 +0530

    Implement Bayesian Dropout layer for uncertainty estimation

---
+++ gnn_optimizer/src/models/uncertainty.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+class BayesianDropout(nn.Module):
+    
+    def __init__(self, p=0.3):
+        super().__init__()
+        self.p = p
+        self.dropout = nn.Dropout(p=p)
+        self.force_on = False # Toggle this to True during uncertainty estimation
+
+    def forward(self, x):
+        # If we are training, OR if we forced it on (for uncertainty), use dropout
+        if self.training or self.force_on:
+            return self.dropout(x)
+        return x
+
+    def enable_mc_dropout(self):
+        self.force_on = True
+
+    def disable_mc_dropout(self):
+        self.force_on = False
